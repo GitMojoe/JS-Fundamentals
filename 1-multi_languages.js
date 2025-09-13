faef13a65099bfc5ad515d7babb055bc078f4162
GitMojoe <greatestmojoe.com>
2025-09-13 23:39:45 +0000

    update

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,4 @@
-console.log("C is fun")
-console.log("Python is cool");
-console.log("Javascript is amazing");
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const msg of messages) {
+  console.log(msg);
+}
