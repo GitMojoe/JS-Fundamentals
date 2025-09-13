6867a68581a06a9456a930ecae0674a78af58076
GitMojoe <greatestmojoe.com>
2025-09-13 23:29:06 +0000

    update

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,4 @@
-let a = "C is fun";
-let b = "Python is cool";
-let c = "Javascript is amazing";
-
-console.log(a);
-console.log(b);
-console.log(c);
+let a = ["C is fun","Python is cool","Javascript is amazing"]
+for(let i=0; i<a.length; i++){
+    console.log(a[i])
+}
