18692ada630020969a8b86792511ec509a9209d8
GitMojoe <greatestmojoe.com>
2025-09-14 21:06:07 +0000

    update

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,26 @@
-const args =process.argv.slice(2);
+// const args =process.argv.splice(2);
 
-let num1 = Number(parseInt(args[0]))
+// let num1 = Number(parseInt(args[0]))
 
-if(num1.isInteger===false || isNaN(num1){
+/*if(num1.isInteger===false || isNaN(num1)){
 console.log("Missing number of occurences")
 
 }else{
-   for(let i =0; i<num1; i++){
-    console.log("C is fun")
-   }
+   num1 +=Number(parseInt(num1))
+   args.forEach(element => {
+      console.log("C is fun")
+   });
+}
+*/
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
