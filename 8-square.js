b9c15b5e240f7750d5e7935b5764d2cc0da27289
Mojoe <125026250+GitMojoe@users.noreply.github.com>
2025-09-14 15:26:45 +0000

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,9 @@
 const args = process.argv.splice(2)
 let num1 = parseInt(args[0])
-if(num1>1){
+if(num1.isInteger ===false || isNaN(num1)){
+    console.log(`Missing size`)
+} else {
     for(let i =0; i<num1; i++){
-    console.log("XX")
+    console.log(`xx`)
 }
-}else{
-    console.log("Missing size")
 }
