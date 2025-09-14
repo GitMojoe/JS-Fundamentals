b89824f575c4cba7912f9183cea357893ccdb9b7
GitMojoe <greatestmojoe.com>
2025-09-14 09:50:42 +0000

    update

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,10 +1,4 @@
-const myArray =["C is fun","Python is cool", "Javascript is amazing"];
+const myArray =["C is fun","Python is cool", "JavaScript is amazing"];
 myArray.forEach(element => {
     console.log(element)
 });
-
-
-// const myArray =["C if fun","Python is cool", "Javascript is amazing"];
-// for(let i =0; i<myArray.length; i++){
-//     console.log(myArray[i])
-// };
