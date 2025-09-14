e6e5bf6fce3baaefd393d8addb7d8bd918c1e187
GitMojoe <greatestmojoe.com>
2025-09-14 10:14:25 +0000

    updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,11 @@
 const args =process.argv;
 
 let num1 = Number(parseInt(args[2]))
+
 if(Number.isInteger(num1)){
 for(let i=0; i<num1; i++){
     console.log("C is fun");
 }
 }else{
-    console.log("Not a number");
+    console.log("Missing number of occurences")
 }
