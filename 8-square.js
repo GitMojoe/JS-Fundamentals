1cfc5e195f924401c0069331b81c96354106cc26
Mojoe <125026250+GitMojoe@users.noreply.github.com>
2025-09-21 17:09:18 +0000

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,13 +1,9 @@
 const size = parseInt(process.argv[2]);
 
-function printRow(n, count = 0) {
-  if (count >= n) return;
-  console.log('X'.repeat(n));
-  printRow(n, count + 1);
-}
-
 if (isNaN(size)) {
-  console.log('Missing size');
+    console.log('Missing size');
 } else {
-  printRow(size);
+    for (let i = 0; i < size; i++) {
+        console.log('X'.repeat(size));
+    }
 }
